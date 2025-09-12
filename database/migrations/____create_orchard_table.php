9440f2a348bfee385bff77093a859e76d9a51769
r02092 <87572333+r02092@users.noreply.github.com>
2025-09-12 21:06:50 +0900

    テーブルのマイグレーションファイルの作成

---
+++ database/migrations/____create_orchard_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::create('orchard', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->unsignedInteger('lng0');
+            $table->unsignedInteger('lat0');
+            $table->unsignedInteger('lng1');
+            $table->unsignedInteger('lat1');
+            $table->unsignedInteger('lng2');
+            $table->unsignedInteger('lat2');
+            $table->unsignedInteger('lng3');
+            $table->unsignedInteger('lat3');
+            $table->unsignedTinyInteger('latin_num');
+            $table->unsignedTinyInteger('digit_num');
+        });
+    }
+    public function down(): void
+    {
+        Schema::dropIfExists('orchard');
+    }
+};
