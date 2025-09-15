24441429de252ce9a5d7b3abc8e239c027c710b2
r02092 <87572333+r02092@users.noreply.github.com>
2025-09-15 17:32:54 +0900

    $orchardsが無くviewでエラーが起きるため、リダイレクトするように
    フォームの再送信対策も兼ねて

--- app/Http/Controllers/RootController.php
+++ app/Http/Controllers/RootController.php
@@ -73,6 +73,6 @@ class RootController extends Controller
             $tree->leaf_area = $values[3];
             $tree->save();
         }
-        return view('root');
+        return redirect('/');
     }
 }
